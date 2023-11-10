7b165774e2dad7e0f32f05a794b2968c894a2b43
Andres Avilan <ingenieroandresmauricio@gmail.com>
2023-11-10 15:32:42 -0500

    arreglando esta monda

--- src/api/index.js
+++
@@ -1,9 +0,0 @@
-import axios from "axios"
-const BASE_URL = import.meta.env.VITE_BASE_API_URL
-
-const getTagsByCategory = async (category) => {
-    const data = await axios.get(`${BASE_URL}/api/tags?populate=%2A&sort[1]=id&filters[category][name][$eq]=${category}`)
-    return data
-}
-
-export { getTagsByCategory }
--- src/hooks/index.js
+++
@@ -1,4 +0,0 @@
-import useScreenSize from "./useScreenSize";
-import usePrevious from "./usePrevious";
-import useDeviceDetect from "./useDeviceDetect";
-export { useScreenSize, usePrevious, useDeviceDetect };
